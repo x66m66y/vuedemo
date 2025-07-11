b411cdf1cd987a2ac564f18c28ff947f94e1b98b
Xu Mingyang <62458045+x66m66y@users.noreply.github.com>
2025-07-11 18:20:24 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="徐铭洋 Welcome to Your Vue.js App"/>
 </template>
 
 <script>
